21ea18d3bc534709490da3a4adbb2d26d4b9a9af
NatanJMai <natan.mai@hotmail.com>
2014-11-05 00:16:04 -0200

    Update work.

---
+++ js/runFile.js
@@ -0,0 +1,47 @@
+function run_cmd(cmd, args, cb, end){
+    child.stdout.on('end', end);
+}
+
+function run_cmd(cmd, args, cb, end) {
+    var spawn = require('child_process').spawn,
+        child = spawn(cmd, args),
+        me    = this;
+    
+    child.stdout.on('data', function (buffer) { cb(me, buffer) });
+    child.stdout.on('end', end);
+
+    child.on('exit', function (code) {
+        var cod = code;
+
+        console.log('child process exited with code ' + cod);
+    });
+}
+
+function run(str){
+    var foo = new run_cmd(
+        './Files/helloworld', [str],
+
+        function (me, buffer) { 
+            me.stdout += buffer.toString() 
+            var vt     = parseInt(buffer.toString())
+            
+            switch(vt){
+                case 1:
+                    console.log("UM")
+                    break;
+
+                case 2:
+                    console.log("DOIS")
+                    break;
+
+                case 3:
+                    console.log("TRES")
+                    break;
+                }
+            },
+        
+        function () { 
+            //console.log(foo.stdout) 
+        }
+    );
+}
